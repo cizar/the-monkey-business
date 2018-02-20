889ac2e946467ef39bfafb4c83d64ed66e2bafc4
Cesar Kastli <cesarkastli@gmail.com>
2018-02-20 20:08:04 -0300

    fix

--- src/app/app.js
+++ src/app/app.js
@@ -1,12 +1,12 @@
 var React = require('react');
 var Router = require('react-router');
-var injectTapEventPlugin = require("react-tap-event-plugin");
+// var injectTapEventPlugin = require("react-tap-event-plugin");
 var Routes = require('./routes');
 
 // Needed for onTouchTap
 // Can go away when react 1.0 release
 // Check this repo: https://github.com/zilverline/react-tap-event-plugin
-injectTapEventPlugin();
+// injectTapEventPlugin();
 
 // Render the app!
 Router.run(Routes, Router.HistoryLocation, function(Handler) {
